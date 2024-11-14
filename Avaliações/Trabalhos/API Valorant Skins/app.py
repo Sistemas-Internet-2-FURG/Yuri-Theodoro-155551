b72928df7c4ac483c7117bbbc066d0e61528e10a
Yuri Theodoro <110777621+yurikpz@users.noreply.github.com>
2024-11-14 17:45:00 -0300

    API CRUD SKINS VALORANT

--- Avaliações/Trabalhos/API Valorant Skins/app.py
+++ Avaliações/Trabalhos/API Valorant Skins/app.py
@@ -1,23 +1,28 @@
+import os
 import sqlite3
-from flask import Flask, jsonify, request, abort
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask import Flask, jsonify, request, abort, render_template
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 #Criação do Flask
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-#Configuração do Flask-Login
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'  #Página de login quando não autenticado
+import secrets
+
+app.secret_key = secrets.token_hex(16)
+
+#Configuração do JWT
+jwt = JWTManager(app)
+
+app.config['DATABASE'] = os.path.join(app.instance_path, 'database.db')
+os.makedirs(app.instance_path, exist_ok=True)
 
 #Conexão com o banco de dados
 def get_db_connection():
-    conn = sqlite3.connect('database.db')
+    conn = sqlite3.connect(app.config['DATABASE'])
     conn.row_factory = sqlite3.Row
     return conn
 
-#Criar tabelas
+#Criação das tabelas
 def create_tables():
     conn = get_db_connection()
     conn.execute('''CREATE TABLE IF NOT EXISTS users (
@@ -41,50 +46,33 @@ def create_tables():
     conn.commit()
     conn.close()
 
-#Classe User para gerenciar autenticação
-class User(UserMixin):
-    def __init__(self, id, username, password):
-        self.id = id
-        self.username = username
-        self.password = password
+#Rota index
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-    def get_id(self):
-        return str(self.id)
-    
-#Carregar usuário pelo ID
-@login_manager.user_loader
-def load_user(user_id):
-    conn = get_db_connection()
-    user = conn.execute('SELECT * FROM users WHERE id = ?', (user_id,)).fetchone()
-    conn.close()
-    if user is None:
-        return None
-    return User(user['id'], user['username'], user['password'])
 
-#Rotas para usuários
-#Rota para login
 @app.route('/login', methods=['POST'])
 def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-
+    print("Credenciais recebidas - Username:", username, "Password:", password)
+    
     conn = get_db_connection()
     user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
     conn.close()
 
     if user and check_password_hash(user['password'], password):
-        login_user(User(user['id'], user['username'], user['password']))
-        return jsonify({"message": "Login bem-sucedido!"}), 200
+        #Gerando token 
+        token = create_access_token(identity=username)
+        return jsonify(token=token), 200
     else:
-        return jsonify({"error": "Login inválido."}), 401
-#Rota para logout
-@app.route('/logout', methods=['POST'])
-@login_required
-def logout():
-    logout_user()
-    return jsonify({"message": "Logout realizado com sucesso!"}), 200
-#Rota para registro de usuários
+        
+        print("Falha na autenticação: Credenciais inválidas")
+        return jsonify(error="Usuário ou senha incorretos"), 401
+
+#Rota registro
 @app.route('/register', methods=['POST'])
 def register():
     data = request.get_json()
@@ -95,24 +83,31 @@ def register():
         return jsonify({"error": "Nome de usuário e senha são obrigatórios."}), 400
 
     conn = get_db_connection()
+    
+    #Verifica se o usuário já existe
+    existing_user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+    if existing_user:
+        conn.close()
+        return jsonify({"error": "Nome de usuário já está em uso."}), 409
+
     hashed_password = generate_password_hash(password)
     conn.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, hashed_password))
     conn.commit()
     conn.close()
+    
     return jsonify({"message": "Usuário registrado com sucesso!"}), 201
 
-#Rotas para coleções
-#Rota para listar coleções
+#Rota coleções
 @app.route('/colecoes', methods=['GET'])
-@login_required
+@jwt_required()
 def list_colecoes():
     conn = get_db_connection()
     colecoes = conn.execute('SELECT * FROM colecoes').fetchall()
     conn.close()
     return jsonify([dict(colecao) for colecao in colecoes]), 200
-#Rota para adicionar nova coleção
+
 @app.route('/colecoes', methods=['POST'])
-@login_required
+@jwt_required()
 def add_colecao():
     data = request.get_json()
     nome = data.get('nome')
@@ -125,9 +120,9 @@ def add_colecao():
     conn.commit()
     conn.close()
     return jsonify({"message": "Coleção adicionada com sucesso!"}), 201
-#Rota para editar coleção
+
 @app.route('/colecoes/<int:id>', methods=['PUT'])
-@login_required
+@jwt_required()
 def edit_colecao(id):
     data = request.get_json()
     nome = data.get('nome')
@@ -140,9 +135,9 @@ def edit_colecao(id):
     conn.commit()
     conn.close()
     return jsonify({"message": "Coleção atualizada com sucesso!"}), 200
-#Rota para deletar coleção
+
 @app.route('/colecoes/<int:id>', methods=['DELETE'])
-@login_required
+@jwt_required()
 def delete_colecao(id):
     conn = get_db_connection()
     conn.execute('DELETE FROM colecoes WHERE id = ?', (id,))
@@ -150,10 +145,9 @@ def delete_colecao(id):
     conn.close()
     return jsonify({"message": "Coleção deletada com sucesso!"}), 200
 
-#Rotas para skins
-#Rota para listar skins
+#Rota skins
 @app.route('/skins', methods=['GET'])
-@login_required
+@jwt_required()
 def list_skins():
     colecao_id = request.args.get('colecao_id')
     nome_arma = request.args.get('nome_arma', '')
@@ -179,9 +173,8 @@ def list_skins():
     conn.close()
     return jsonify([dict(skin) for skin in skins]), 200
 
-#Rota para adicionar nova skin
 @app.route('/skins', methods=['POST'])
-@login_required
+@jwt_required()
 def add_skin():
     data = request.get_json()
     nome = data.get('nome')
@@ -196,9 +189,8 @@ def add_skin():
     conn.close()
     return jsonify({"message": "Skin adicionada com sucesso!"}), 201
 
-#Rota para editar skin
 @app.route('/skins/<int:id>', methods=['PUT'])
-@login_required
+@jwt_required()
 def edit_skin(id):
     data = request.get_json()
     nome = data.get('nome')
@@ -212,18 +204,19 @@ def edit_skin(id):
     conn.commit()
     conn.close()
     return jsonify({"message": "Skin atualizada com sucesso!"}), 200
-#Rota para deletar skin
+
 @app.route('/skins/<int:id>', methods=['DELETE'])
-@login_required
+@jwt_required()
 def delete_skin(id):
     conn = get_db_connection()
     conn.execute('DELETE FROM skins WHERE id = ?', (id,))
     conn.commit()
     conn.close()
     return jsonify({"message": "Skin deletada com sucesso!"}), 200
-#Cria tabelas ao iniciar a aplicação
+
+#Cria tabelas 
 create_tables()
 
-#Inicia servidor 
+#Inicia servidor
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
